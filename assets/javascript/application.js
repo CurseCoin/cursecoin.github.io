b2b0aac66c01b5750812b55e11016557243aa98c
Alexander Leitner <leitnersalex@gmail.com>
2018-09-04 15:16:48 -0400

    spoopy

---
+++ assets/javascript/application.js
@@ -0,0 +1,23 @@
+const rinkeby =  {
+  address: "0x0",
+  name: "Curse Coin",
+  json: "[]"
+};
+
+const mainnet =  {
+  address: "0x0",
+  name: "Curse Coin",
+  json: "[]"
+};
+
+const ropsten =  {
+  address: "0x0",
+  name: "Curse Coin",
+  json: "[]"
+};
+
+const kovan =  {
+  address: "0x0",
+  name: "Curse Coin",
+  json: "[]"
+};
